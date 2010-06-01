3d222553c43661a75dccf7bf02f91e5535ac26f6
David Jones <davidthomjones@gmail.com>
2010-06-01 13:59:20 +1000

    generate opml support

--- coordinator/view.php
+++ coordinator/view.php
@@ -12,8 +12,7 @@ require_once($CFG->dirroot.'/mod/bim/coordinator/find_student.php');
  * - show coordinator interface
  */
 
-function show_coordinator( $bim, $userid, $cm, $course )
-{
+function show_coordinator( $bim, $userid, $cm, $course ) {
 
     // optional params required to chose screen/tab
     $tab = optional_param('tab', "config", PARAM_ALPHA);
@@ -23,58 +22,55 @@ function show_coordinator( $bim, $userid, $cm, $course )
     // ShowDetails and AllocatePosts then tab is ShowCoordDetails
     if ( $screen == "ShowPostDetails" || $screen == "AllocatePosts" ||
          $screen == "ShowDetails" || $screen == "MarkPost" ||
-         $screen == "changeBlogRegistration" ) {
+         $screen == "changeBlogRegistration" || $screen == "generateOpml" ) {
         $tab = "details";
     }
 
-    bim_print_header( $cm, $bim, $course, $screen );
+    // only generate header of web page for normal HTML pages
+    if ( $screen != "generateOpml" ) {
+        bim_print_header( $cm, $bim, $course, $screen );
 
-    // ** eventually will need to pass screen
-    bim_build_coordinator_tabs( $cm, $tab );
-  
-  if ( $tab == "config" )
-  {
-    bim_configuration_screen( $bim, $cm );
-  }
-  else if ( $tab == "markers" )
-  {
-    bim_allocate_markers( $bim, $cm, $userid );
-  }
-  else if ( $tab == "questions" )
-  {
-    bim_manage_questions( $bim, $cm );
-  }
-  else if ( $tab == "manage" )
-  {
-    $op = optional_param('op', NULL, PARAM_ALPHA);
-    if ( $op == "" )
-      bim_manage_marking( $bim, $userid, $cm );
-    else if ( $op == "release" )
-      bim_manage_release( $bim, $userid, $cm );
-    else if ( $op == "view" )
-      bim_manage_view( $bim, $userid, $cm );
-  }
-  else if ( $tab == "find" )
-  {
-    bim_find_student( $bim, $cm );
-  }
-  else if ( $tab == "details" )
-  {
-    if ( $screen == "ShowDetails" ) {
-        show_marker_student_details( $bim, $userid, $cm );
-    } else if ( $screen == "ShowPostDetails" ) {
-        show_marker_post_details( $bim, $userid, $cm );
-    } else if ( $screen == "AllocatePosts" ) {
-        $student = optional_param('uid', 0, PARAM_INT);
-        bim_marker_allocate_posts( $bim, $userid, $cm, $student );
-    } else if ( $screen == "MarkPost" ) {
-        $marking = optional_param('markingId', 0, PARAM_INT);
-        bim_marker_mark_post( $bim, $userid, $cm, $marking );
-    } else if ( $screen == "changeBlogRegistration" ) {
-        $student = optional_param( 'student', 0, PARAM_INT );
-        bim_change_blog_registration( $bim, $student, $cm );
+        // ** eventually will need to pass screen
+        bim_build_coordinator_tabs( $cm, $tab );
     }
-  }
+  
+    if ( $tab == "config" ) {
+        bim_configuration_screen( $bim, $cm );
+    } else if ( $tab == "markers" ) {
+        bim_allocate_markers( $bim, $cm, $userid );
+    } else if ( $tab == "questions" ) {
+        bim_manage_questions( $bim, $cm );
+    } else if ( $tab == "manage" ) {
+        $op = optional_param('op', NULL, PARAM_ALPHA);
+        if ( $op == "" )
+            bim_manage_marking( $bim, $userid, $cm );
+        else if ( $op == "release" )
+            bim_manage_release( $bim, $userid, $cm );
+        else if ( $op == "view" )
+            bim_manage_view( $bim, $userid, $cm );
+    } else if ( $tab == "find" ) {
+        bim_find_student( $bim, $cm );
+    } else if ( $tab == "details" ) {
+        if ( $screen == "ShowDetails" ) {
+            show_marker_student_details( $bim, $userid, $cm );
+        } else if ( $screen == "ShowPostDetails" ) {
+            show_marker_post_details( $bim, $userid, $cm );
+        } else if ( $screen == "AllocatePosts" ) {
+            $student = optional_param('uid', 0, PARAM_INT);
+            bim_marker_allocate_posts( $bim, $userid, $cm, $student );
+        } else if ( $screen == "MarkPost" ) {
+            $marking = optional_param('markingId', 0, PARAM_INT);
+            bim_marker_mark_post( $bim, $userid, $cm, $marking );
+        } else if ( $screen == "changeBlogRegistration" ) {
+            $student = optional_param( 'student', 0, PARAM_INT );
+            bim_change_blog_registration( $bim, $student, $cm );
+        } else if ( $screen == "generateOpml" ) {
+            bim_generate_opml( $bim, $cm, $userid );
+        }
+    }
+    if ( $screen != "generateOpml" ) {
+        print_footer( $course );
+    }
 }
 
 /*******
@@ -140,4 +136,3 @@ function bim_configuration_screen( $bim, $cm )
 
 
 ?>
-
