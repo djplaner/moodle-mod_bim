9696f91371a41785e73ff79cd885fcd24f013f93
David Jones <davidthomjones@gmail.com>
2010-06-01 12:56:14 +1000

    Got opml string generation going

--- lib/opml.php
+++ lib/opml.php
@@ -6,47 +6,17 @@
 
 
 /*
- * $opml = bim_generate_opml( $structure )
+ * $opml = bim_generate_opml_string( $structure )
  * - given $structure (array of hashes) convert it into
  *   a string in OPML 2.0 format
  */
 
-function bim_generate_opml( $structure ) {
+function bim_generate_opml_string( $structure ) {
     $opml = ""; 
     $header_fields = Array( "title", "dateCreated", "dataModified",
                             "ownerName", "ownerEmail" );
     $item_fields = Array( "text", "description", "htmlUrl", "language",
                           "title", "type", "version", "xmlUrl" );
-// testing
-
-    $structure =
-        Array(
-            head =>
-                Array(
-                    title => "The title of the OPML file",
-                ),
-            items =>
-                Array(
-                    0 => Array(
-                        text => "David's website",
-                        description => "A personal blog",
-                        htmlUrl => "http://davidtjones.wordpress.com/",
-                        xmlUrl => "http://davidtjones.wordpress.com/feed/",
-                        type => "rss",
-                        title => "Weblog of (a) David Jones"
-                    ),
-                    1 => Array(
-                        text => "Indicators",
-                        description => "Indicators site",
-                        htmlUrl => "http://indicatorsproject.wordpress.com/",
-                        xmlUrl => "http://indicatorsproject.wordpress.com/feed/",
-                        type => "rss",
-                        title => "Indicators project"
-                    )
-            )
-        );
-
-//    print_r( $structure );
 
     // generate the intro
     $opml =<<<EOF
