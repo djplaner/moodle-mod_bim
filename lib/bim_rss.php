1d2ce35a343155680fa1c3e5393591d2ce432cc2
David Jones <davidthomjones@gmail.com>
2013-05-26 09:32:32 +1000

    Misc changes

--- lib/bim_rss.php
+++ lib/bim_rss.php
@@ -1,4 +1,4 @@
-<?php    // $Id
+<?php    
 
 /****************
  * Library of functions for manipulating the student feeds
--- lib/locallib.php
+++ lib/locallib.php
@@ -1,4 +1,4 @@
-<?php  // $Id: lib.php,v 1.7.2.5 2009/04/22 21:30:57 skodak Exp $
+<?php  
 
 require_once($CFG->dirroot.'/mod/bim/lib/bim_rss.php');
 /**
